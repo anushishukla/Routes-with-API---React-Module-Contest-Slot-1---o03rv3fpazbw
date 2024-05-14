2b27f9126320ba55ee5898b9b9d6c7505cff0d12
anushishukla <144536251+anushishukla@users.noreply.github.com>
2024-05-14 19:58:16 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,19 @@
+
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className='nav'>
-        <Link className='site-title'>Site Name</Link>
-        <ul>
-            <li>
-                <Link className='top-ten'>Top 10</Link>
-            </li>
-            <li>
-                <Link className='contact'>Contact</Link>
-            </li>
-        </ul>
+    <nav>
+      <h1><Link to="/">Site Name</Link></h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/top-10">Top 10</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
